92faf15445c5003308ff263dd33478e7a767c273
ghassandahik <ghassan.dahik@tu-dortmund.de>
2025-06-04 14:15:57 +0300

    Added path for finding a quote by its ID

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ quotes = [
 def home():
     return "Welcome to the Quote API!!"
 
+# Returns all quotes.
 @app.route("/api/quotes", methods=["GET"])
 def get_quotes():
     return jsonify(quotes)
 
+# Route to get a quote from a specific author. GET method is used to fetch data from a server
 @app.route("/api/quote", methods=["GET"])
 def get_quote_by_author():
     author = request.args.get("author")
@@ -29,6 +31,18 @@ def get_quote_by_author():
 
     return jsonify(results)
 
+# Find a quote by its ID
+@app.route("/api/quote/<int:id>", methods=["GET"])
+def get_quote_by_id(id):
+
+    for quote in quotes:
+        if quote["id"] == id:
+            return jsonify(quote)
+
+    return jsonify({"error": "Quote not found for ID"}), 404
+
+
+# Route to add a new quote. POST method is used to send data to the server.
 @app.route("/api/addquote", methods=["POST"])
 def add_quote():
     data = request.get_json()
@@ -37,7 +51,8 @@ def add_quote():
         return jsonify({"error": "Invalid data. Must include 'author' and 'text'."}), 400
 
     # Generate a new ID (find max ID + 1)
-    # “Loop through every quote in quotes. Try to get the id (use 0 if missing). Find the highest one. Add 1. That’s the new ID.”
+    # “Loop through every quote in quotes. Try to get the id (use 0 if missing). Find the highest one and add 1.
+    # default = 0 incase the list of quotes is empty.
     new_id = max([q.get("id", 0) for q in quotes], default = 0) + 1
 
     new_quote = {
